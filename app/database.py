c42de43b02c46fa57af2526cdd4c5b12b6d9407c
Ayman Nageh <aymannageh028@gmail.com>
2026-05-13 20:13:27 +0300

    latest update

--- app/database.py
+++ app/database.py
@@ -1,22 +1,30 @@
+import os
+
 from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm import declarative_base
 
 
-DATABASE_URL = "mysql+pymysql://root:@localhost/blog_system_db"
+SQLALCHEMY_DATABASE_URL: str = os.getenv(
+
+    "SQLALCHEMY_DATABASE_URL",
+
+    "mysql+pymysql://root:@localhost:3306/blog_system_db"
+)
 
 
 engine = create_engine(
-    DATABASE_URL,
-    echo=True
-)
 
+    SQLALCHEMY_DATABASE_URL
+)
 
 SessionLocal = sessionmaker(
+
     autocommit=False,
+
     autoflush=False,
+
     bind=engine
 )
-
 
 Base = declarative_base()
